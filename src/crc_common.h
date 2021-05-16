805eaddb42b8c2c6c759f4e315819cb8539903a0
Dinamiet <iceman.sm39@gmail.com>
2021-05-16 16:44:22 +0200

    Added common file with constants

---
+++ src/crc_common.h
@@ -0,0 +1,17 @@
+#ifndef __CRC_COMMON__
+#define __CRC_COMMON__
+
+#define CRC8_POLYNOMIAL	 0x07
+#define CRC16_POLYNOMIAL 0x1021
+#define CRC32_POLYNOMIAL 0x04C11DB7
+
+#define CRC8_MSB  (1 << 7)
+#define CRC16_MSB (1 << 15)
+#define CRC32_MSB (1 << 31)
+
+#define CRC16_2_MSB 8
+#define CRC24_2_MSB 24
+
+#define BITS_IN_BYTE 8
+
+#endif
